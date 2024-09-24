51ead8f428a4aba472464009dc4b249d6816db40
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-09-24 13:19:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+    }
+
+    private static void printFactors(int number) {
+        //Check if number is negative. If so return -1
+
+        for(int i = 1; i <= number; i++){
+            //check if number mod i is 0
+            //If so print it use System.out.print() not System.out.println().  Don't forget a space between
+            //If not don't do anything
+        }
+
+
+    }
+}
